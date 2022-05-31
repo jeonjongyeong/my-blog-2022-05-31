c3f3832b869c73cfbc33da1be8d331050e871052
jkkjkhh <jkkjkhh@gmail.com>
2022-05-31 12:37:01 +0900

    test, test-space 페이지 생성

---
+++ src/pages/test-space.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function testSpacePage() {
+  return (
+    <main>
+      <title>test space</title>
+      <h1>테스트 스페이스 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default testSpacePage;
