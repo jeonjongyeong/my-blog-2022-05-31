05ddc7be9bc222b4e1342b53d986a16d3a0f1eda
jkkjkhh <jkkjkhh@gmail.com>
2022-05-31 11:27:40 +0900

    세팅

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Blog 2022-05-31`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
