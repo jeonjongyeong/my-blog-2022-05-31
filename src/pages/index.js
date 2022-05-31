0190985819353028ad2f9a7bfda3a9abc76b7782
jkkjkhh <jkkjkhh@gmail.com>
2022-05-31 12:41:00 +0900

    페이지 링크 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Home Page</h1>
+      <Link to="/about">About</Link>
+      <br />
+      <Link to="/test">test</Link>
+      <br />
+      <Link to="/test-space">testSpace</Link>
     </main>
   );
 };
