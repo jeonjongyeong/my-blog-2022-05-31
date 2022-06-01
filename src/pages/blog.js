ab4f6174d9e60013f42d6fb4c1bedefee87e9327
jkkjkhh <jkkjkhh@gmail.com>
2022-06-01 12:44:19 +0900

    MDX를 사용하여 데이터 렌더링

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,16 +2,19 @@ import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <p>나의 블로그 페이지</p>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
       <StaticImage
         alt="image"
         src="http://www.thedrive.co.kr/news/data/20210923/p1065600010144779_443_thum.jpg"
@@ -22,9 +25,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
